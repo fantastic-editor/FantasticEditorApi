5162bcbe27632fbb81ae0d032ccc9cf1d9a160ed
XiaoLFeng <gm@x-lf.cn>
2024-05-13 15:20:17 +0800

    feat(初始): Initial commit

---
+++ src/main/kotlin/com/frontleaves/fantasticeditor/FantasticEditorApplication.kt
@@ -0,0 +1,11 @@
+package com.frontleaves.fantasticeditor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FantasticEditorApplication
+
+fun main(args: Array<String>) {
+    runApplication<FantasticEditorApplication>(*args)
+}
---
+++ src/test/kotlin/com/frontleaves/fantasticeditor/FantasticEditorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.frontleaves.fantasticeditor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FantasticEditorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
