88ab56c2041e82b1a483bb9e888703e8bad8aad7
xiangZr-hhh <2774915446@qq.com>
2024-06-05 10:20:18 +0800

    patch(补丁):角色查询接口
    修正了部分代码规范问题

--- src/main/kotlin/com/frontleaves/fantasticeditor/services/RoleServiceImpl.java
+++ src/main/kotlin/com/frontleaves/fantasticeditor/services/RoleServiceImpl.java
@@ -1,14 +1,10 @@
 /*
- * *******************************************************************************
- * Copyright (C) 2024-NOW(至今) 妙笔智编
+ * ******************************************************************************* Copyright (C) 2024-NOW(至今) 妙笔智编
  * Author: 锋楪技术团队
  *
- * 本文件包含 妙笔智编「FantasticEditor」 的源代码，该项目的所有源代码均遵循MIT开源许可证协议。
- * 本代码仅允许在十三届软件杯比赛授权比赛方可直接使用
- * *******************************************************************************
- * 免责声明：
- * 使用本软件的风险由用户自担。作者或版权持有人在法律允许的最大范围内，
- * 对因使用本软件内容而导致的任何直接或间接的损失不承担任何责任。
+ * 本文件包含 妙笔智编「FantasticEditor」 的源代码，该项目的所有源代码均遵循MIT开源许可证协议。 本代码仅允许在十三届软件杯比赛授权比赛方可直接使用
+ * ******************************************************************************* 免责声明：
+ * 使用本软件的风险由用户自担。作者或版权持有人在法律允许的最大范围内， 对因使用本软件内容而导致的任何直接或间接的损失不承担任何责任。
  * *******************************************************************************
  */
 
@@ -25,11 +21,8 @@ import com.frontleaves.fantasticeditor.services.interfaces.RoleService;
 import com.frontleaves.fantasticeditor.utility.ErrorCode;
 import com.frontleaves.fantasticeditor.utility.Util;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import lombok.RequiredArgsConstructor;
-import org.apache.avro.data.Json;
 import org.jetbrains.annotations.NotNull;
-import org.mortbay.util.ajax.JSON;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -53,10 +46,7 @@ public class RoleServiceImpl implements RoleService {
     private final Gson gson;
 
     @Override
-    public boolean editCustomRole(
-            @NotNull final RoleCustomEditVO roleCustomEditVO,
-            @NotNull final String roleId
-    ) {
+    public boolean editCustomRole(@NotNull final RoleCustomEditVO roleCustomEditVO, @NotNull final String roleId) {
         SqlRoleDO getRole = roleDAO.getRoleByRUUID(roleId);
         ArrayList<String> dontEditRole = new ArrayList<>();
         dontEditRole.add("console");
@@ -72,12 +62,10 @@ public class RoleServiceImpl implements RoleService {
                 throw new BusinessException("角色名称已存在", ErrorCode.OPERATION_FAILED);
             }
             // 更新角色信息
-            getRole
-                    .setName(roleCustomEditVO.getName())
-                    .setDisplayName(roleCustomEditVO.getDisplayName())
-                    .setDescription(roleCustomEditVO.getDescription())
-                    .setUpdatedAt(new Timestamp(System.currentTimeMillis()))
-                    .setPermissions(gson.toJson(roleCustomEditVO.getPermissions()));
+            getRole.setName(roleCustomEditVO.getName()).setDisplayName(roleCustomEditVO.getDisplayName())
+                .setDescription(roleCustomEditVO.getDescription())
+                .setUpdatedAt(new Timestamp(System.currentTimeMillis()))
+                .setPermissions(gson.toJson(roleCustomEditVO.getPermissions()));
             return roleDAO.updateById(getRole);
         } else {
             throw new BusinessException("角色不存在", ErrorCode.OPERATION_FAILED);
@@ -91,124 +79,114 @@ public class RoleServiceImpl implements RoleService {
         if (checkRole != null) {
             throw new BusinessException("角色名称已存在", ErrorCode.OPERATION_FAILED);
         }
-        SqlRoleDO newRole = new SqlRoleDO()
-                .setRuuid(Util.INSTANCE.makeNoDashUUID())
-                .setName(roleCustomEditVO.getName())
-                .setDisplayName(roleCustomEditVO.getDisplayName())
-                .setDescription(roleCustomEditVO.getDescription())
-                .setUpdatedAt(new Timestamp(System.currentTimeMillis()))
-                .setPermissions(gson.toJson(roleCustomEditVO.getPermissions()));
+        SqlRoleDO newRole = new SqlRoleDO().setRuuid(Util.INSTANCE.makeNoDashUUID()).setName(roleCustomEditVO.getName())
+            .setDisplayName(roleCustomEditVO.getDisplayName()).setDescription(roleCustomEditVO.getDescription())
+            .setUpdatedAt(new Timestamp(System.currentTimeMillis()))
+            .setPermissions(gson.toJson(roleCustomEditVO.getPermissions()));
         return roleDAO.addRole(newRole);
     }
 
-
     @Override
-    public List<RoleInfoVO> getUserRoleInfo(@NotNull final List<String> uuids,
-                                            @NotNull final List<String> usernames,
-                                            @NotNull final List<String> ruuids,
-                                            @NotNull final List<String> roleNames) {
+    public List<RoleInfoVO> getUserRoleInfo(@NotNull final List<String> uuids, @NotNull final List<String> usernames,
+        @NotNull final List<String> ruuids, @NotNull final List<String> roleNames) {
 
-        List<RoleInfoVO> roleInfoVOS = new ArrayList<>();
+        List<RoleInfoVO> roleInfoVoS = new ArrayList<>();
 
         // 根据用户uuid获取角色信息
         for (String uuid : uuids) {
-            //  根据uuid获取用户信息
+            // 根据uuid获取用户信息
             SqlUserDO sqlUserDO = userDAO.getUserByUUID(uuid);
             if (sqlUserDO == null) {
                 continue;
             }
 
-            //  根据用户的ruuid查询角色
-            if (sqlUserDO.getRole() != null || !sqlUserDO.getRole().isEmpty()) {
-                //  封装数据
-                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(sqlUserDO.getRole(),"ruuid");
+            // 根据用户的ruuid查询角色
+            if (sqlUserDO.getRole() != null) {
+                // 封装数据
+                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(sqlUserDO.getRole(), "ruuid");
                 if (roleInfoVO != null) {
-                    roleInfoVOS.add(roleInfoVO);
+                    roleInfoVoS.add(roleInfoVO);
                 }
             }
         }
 
         // 根据用户名获取角色信息
-        for (String username: usernames) {
-            //  根据username获取用户信息
+        for (String username : usernames) {
+            // 根据username获取用户信息
             SqlUserDO sqlUserDO = userDAO.getUserByUsername(username);
             if (sqlUserDO == null) {
                 continue;
             }
 
-            //  根据用户的ruuid查询角色
-            if (sqlUserDO.getRole() != null || !sqlUserDO.getRole().isEmpty()) {
-                //  封装数据
-                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(sqlUserDO.getRole(),"ruuid");
+            // 根据用户的ruuid查询角色
+            if (sqlUserDO.getRole() != null) {
+                // 封装数据
+                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(sqlUserDO.getRole(), "ruuid");
                 if (roleInfoVO != null) {
-                    roleInfoVOS.add(roleInfoVO);
+                    roleInfoVoS.add(roleInfoVO);
                 }
             }
         }
 
         // 根据角色id获取角色信息
-        for (String ruuid: ruuids) {
-            //  根据用户的ruuid查询角色
-            if (ruuid != null || !ruuid.isEmpty()) {
-                //  封装数据
-                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(ruuid,"ruuid");
+        for (String ruuid : ruuids) {
+            // 根据用户的ruuid查询角色
+            if (ruuid != null) {
+                // 封装数据
+                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(ruuid, "ruuid");
                 if (roleInfoVO != null) {
-                    roleInfoVOS.add(roleInfoVO);
+                    roleInfoVoS.add(roleInfoVO);
                 }
             }
         }
 
         // 根据角色名称获取角色信息
-        for (String roleName: roleNames) {
-            //  根据用户的ruuid查询角色
-            if (roleName != null || !roleName.isEmpty()) {
-                //  封装数据
-                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(roleName,"roleName");
+        for (String roleName : roleNames) {
+            // 根据用户的ruuid查询角色
+            if (roleName != null) {
+                // 封装数据
+                RoleInfoVO roleInfoVO = encapsulateRoleByIdOrName(roleName, "roleName");
                 if (roleInfoVO != null) {
-                    roleInfoVOS.add(roleInfoVO);
+                    roleInfoVoS.add(roleInfoVO);
                 }
             }
         }
 
-        return roleInfoVOS;
+        return roleInfoVoS;
     }
 
+    private final static String ROLE_TYPE_ROLE_NAME = "roleName";
+    private final static String ROLE_TYPE_ROLE_ID = "ruuid";
 
     /**
-     * ## 封装角色信息
-     * 根据ruuid或角色名称查询到角色数据，然后进行封装
+     * ## 封装角色信息 根据ruuid或角色名称查询到角色数据，然后进行封装
      *
      * @param data 要查询的数据
      * @param type 查询的类型（ruuid、roleName）
      * @return 角色信息实体类
      */
-    private RoleInfoVO encapsulateRoleByIdOrName (String data,
-                                                  String type) {
-        //  查询角色数据
+    private RoleInfoVO encapsulateRoleByIdOrName(String data, String type) {
+        // 查询角色数据
         SqlRoleDO sqlRoleDO = null;
-        if (type.equals("ruuid")) {
+        if (ROLE_TYPE_ROLE_NAME.equals(type)) {
             sqlRoleDO = roleDAO.getRoleByRUUID(data);
-        } else if (type.equals("roleName")) {
+        } else if (ROLE_TYPE_ROLE_ID.equals(type)) {
             sqlRoleDO = roleDAO.getRoleByRoleName(data);
         }
-        //  未查询，返回null
+        // 未查询，返回null
         if (sqlRoleDO == null) {
             return null;
         }
-        //  封装数据
+        // 封装数据
         RoleInfoVO roleInfoVO = new RoleInfoVO();
         Util.INSTANCE.copyPropertiesData(sqlRoleDO, roleInfoVO);
-        if (sqlRoleDO.getPermissions() != "[]") {
-            roleInfoVO.setPermissions(gson.fromJson(sqlRoleDO.getPermissions(),String[].class));
+        if (!sqlRoleDO.getPermissions().equals(gson.toJson(new String[0]))) {
+            roleInfoVO.setPermissions(gson.fromJson(sqlRoleDO.getPermissions(), String[].class));
         } else {
             roleInfoVO.setPermissions(new String[0]);
         }
 
-        return  roleInfoVO;
+        return roleInfoVO;
     }
-
-
-
-
 
 }
