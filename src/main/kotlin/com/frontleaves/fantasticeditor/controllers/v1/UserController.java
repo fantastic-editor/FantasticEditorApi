f0357357c5bd2e8ae57927d4da5bc82b4c025fbb
xiangZr-hhh <2774915446@qq.com>
2024-06-07 08:08:53 +0800

    patch(补丁):根据uuid获取用户信息
    修正了部分代码规范问题

--- src/main/kotlin/com/frontleaves/fantasticeditor/controllers/v1/UserController.java
+++ src/main/kotlin/com/frontleaves/fantasticeditor/controllers/v1/UserController.java
@@ -21,7 +21,6 @@ import com.frontleaves.fantasticeditor.services.interfaces.UserService;
 import com.frontleaves.fantasticeditor.utility.BaseResponse;
 import com.frontleaves.fantasticeditor.utility.ErrorCode;
 import com.frontleaves.fantasticeditor.utility.ResultUtil;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -33,72 +32,72 @@ import org.springframework.web.bind.annotation.*;
  * <hr>
  * 用户控制器，用于处理用户相关的请求;
  *
- * @since v1.0.0
- * @version v1.0.0
  * @author xiao_lfeng
+ * @version v1.0.0
+ * @since v1.0.0
  */
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
-    private final UserService userService;
+	private final UserService userService;
 
-    /**
-     * 邮箱验证
-     * <hr>
-     * 用户邮箱验证的地址，用于验证用户的邮箱是否合法；邮箱验证码的发送会根据用户在注册后，执行 authResendMailVerify 方法时的邮箱地址发送；
-     * 验证码的有效时间为 15 分钟；获取到的验证码在当前接口进行验证；
-     *
-     * @return 注册结果
-     */
-    @PostMapping("/mail/verify")
-    public ResponseEntity<BaseResponse<Void>> userMailVerify(
-            @RequestBody @Validated final UserMailVerifyVO userMailVerifyVO
-    ) {
-        userService.checkMailVerify(userMailVerifyVO.getEmail(), userMailVerifyVO.getVerifyCode());
-        return ResultUtil.success("验证成功");
-    }
+	/**
+	 * 邮箱验证
+	 * <hr>
+	 * 用户邮箱验证的地址，用于验证用户的邮箱是否合法；邮箱验证码的发送会根据用户在注册后，执行 authResendMailVerify 方法时的邮箱地址发送；
+	 * 验证码的有效时间为 15 分钟；获取到的验证码在当前接口进行验证；
+	 *
+	 * @return 注册结果
+	 */
+	@PostMapping("/mail/verify")
+	public ResponseEntity<BaseResponse<Void>> userMailVerify(
+			@RequestBody @Validated final UserMailVerifyVO userMailVerifyVO
+	) {
+		userService.checkMailVerify(userMailVerifyVO.getEmail(), userMailVerifyVO.getVerifyCode());
+		return ResultUtil.success("验证成功");
+	}
 
-    /**
-     * 重发邮件验证码
-     * <hr>
-     * 用于用户在注册时，未收到验证码或者验证码失效时，重新发送验证码；
-     *
-     * @param email 邮箱地址
-     * @return 重发结果
-     */
-    @GetMapping("/mail/resend")
-    public ResponseEntity<BaseResponse<Void>> authResendMailVerify(
-            @RequestParam("email") final String email
-    ) {
-        if (email.matches("^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")) {
-            userService.sendMailVerify(email);
-            return ResultUtil.success("发送成功");
-        } else {
-            throw new BusinessException("邮箱格式错误", ErrorCode.REQUEST_BODY_PARAMETERS_ERROR);
-        }
-    }
+	/**
+	 * 重发邮件验证码
+	 * <hr>
+	 * 用于用户在注册时，未收到验证码或者验证码失效时，重新发送验证码；
+	 *
+	 * @param email 邮箱地址
+	 * @return 重发结果
+	 */
+	@GetMapping("/mail/resend")
+	public ResponseEntity<BaseResponse<Void>> authResendMailVerify(
+			@RequestParam("email") final String email
+	) {
+		if (email.matches("^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")) {
+			userService.sendMailVerify(email);
+			return ResultUtil.success("发送成功");
+		} else {
+			throw new BusinessException("邮箱格式错误", ErrorCode.REQUEST_BODY_PARAMETERS_ERROR);
+		}
+	}
 
-    /**
-     * 通过uuid获取用户信息
-     * <hr>
-     * 根据uuid参数查询用户的基本信息，信息包括：角色组信息，权限信息，付费会员信息，以及个人其他信息
-     *
-     * @param uuid 用户uuid
-     * @return 用户公开信息
-     */
-    @GetMapping("/info/get/ByUUID/{uuid}")
-    public ResponseEntity<BaseResponse<UserPublicInfoVO>> getMyUserProfileInfo(
-            @PathVariable("uuid") final String uuid
-    ) {
+	/**
+	 * 通过uuid获取用户信息
+	 * <hr>
+	 * 根据uuid参数查询用户的基本信息，信息包括：角色组信息，权限信息，付费会员信息，以及个人其他信息
+	 *
+	 * @param uuid 用户uuid
+	 * @return 用户公开信息
+	 */
+	@GetMapping("/info/get/ByUUID/{uuid}")
+	public ResponseEntity<BaseResponse<UserPublicInfoVO>> getMyUserProfileInfo(
+			@PathVariable("uuid") final String uuid
+	) {
 
-        if (uuid == null || uuid.isEmpty()) {
-            throw new BusinessException("uuid参数为空",ErrorCode.REQUEST_PARAMETERS_ERROR);
-        }
+		if (uuid == null || uuid.isEmpty()) {
+			throw new BusinessException("uuid参数为空", ErrorCode.REQUEST_PARAMETERS_ERROR);
+		}
 
-        UserPublicInfoVO userVO = userService.getMyUserProfileInfo(uuid);
-        return ResultUtil.success("获取当前用户信息成功",userVO);
-    }
+		UserPublicInfoVO userVO = userService.getMyUserProfileInfo(uuid);
+		return ResultUtil.success("获取当前用户信息成功", userVO);
+	}
 
 }
